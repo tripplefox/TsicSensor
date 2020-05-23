853156b70d8d577a8e3933c5aa9cc63ba076ce3c
TrippleFox <65678634+tripplefox@users.noreply.github.com>
2020-05-23 18:42:16 +0200

    correct comment

--- examples/basicAsyncRead/basicAsyncRead.ino
+++ examples/basicAsyncRead/basicAsyncRead.ino
@@ -18,8 +18,8 @@ void setup()
    // This creates/initializes a TSIC_506 sensor connected to GPIO 16.
    // -----------------------------------------------------------------------------------
    // The sensor is configured with "TsicExternalVcc", so it has a permanent external 
-   // power source. The sensor values are then read in the background. Check for new values
-   // with the "newValueAvailable()" function...
+   // power source. The sensor values are then read in the background. 
+   // (we can check for new values with the "newValueAvailable()" function...)
    
    sensor1 = TsicSensor::create(16, TsicExternalVcc, TsicType::TSIC_506);
 }
@@ -30,11 +30,11 @@ void loop()
 	// we are now waiting for new (changed) temperature values ...
 	if(sensor1->newValueAvailable())
     {      
-	  // Get the temerature in °Celsius.
+	  // Get the temperature in °Celsius.
       temperature = sensor1->getTempCelsius();
     }
 
-    // output the temerature value to the serial port...
+    // output the temperature value to the serial port...
     Serial.println(temperature);
     delay(100);
 }
